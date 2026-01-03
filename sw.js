a529c7e452916712583439290c38c09570b916ac
Zagerous <93443703+Kylemaddenar@users.noreply.github.com>
2026-01-03 08:14:40 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 /*
  * Player Almanac - Service Worker
- * Version: 7.0.1
+ * Version: 7.1.0
  * 
  * This service worker caches the app for full offline functionality.
  * After the first visit, the app works completely without internet.
  */
 
-const CACHE_NAME = 'player-almanac-v7.0.1';
+const CACHE_NAME = 'player-almanac-v7.1.0';
 
 // Files to cache for offline use
 const CACHE_FILES = [
@@ -16,7 +16,7 @@ const CACHE_FILES = [
 ];
 
 self.addEventListener('install', (event) => {
-  console.log('[ServiceWorker] Installing v7.0.1...');
+  console.log('[ServiceWorker] Installing v7.1.0...');
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return Promise.allSettled(
@@ -27,7 +27,7 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('[ServiceWorker] Activating v7.0.1...');
+  console.log('[ServiceWorker] Activating v7.1.0...');
   event.waitUntil(
     caches.keys().then((cacheNames) =>
       Promise.all(
